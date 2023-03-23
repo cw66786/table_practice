6fe21824432a4156c23c6931bc96207b24bb2d26
Craig Wilson <cw66786@gmail.com>
2023-03-23 15:34:00 -0500

    added sort ability and hover effects for better ui

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -16,4 +16,12 @@ export class TableComponent implements OnInit {
     this.tableService.getInfo().subscribe(res => this.posts = res)
   }
 
+  sortById(){
+  this.posts = this.posts.sort((a, b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0)
+  }
+
+  sortByTitle(){
+    this.posts = this.posts.sort((a, b) => a.title < b.title ? -1 : a.title > b.title ? 1 : 0)
+    }
+
 }
